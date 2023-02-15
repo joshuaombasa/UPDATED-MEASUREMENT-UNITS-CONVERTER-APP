7f1c04cb191edd0e85a28232d69676e06183cd50
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-02-15 21:16:08 +0300

    updated program logic by adding unitsInput variable to all functions

--- index.js
+++ index.js
@@ -4,13 +4,14 @@
 1 kilogram = 2.204 pound
 */
 
-const unitsInput = document.getElementById("units-display-area").value
+
 const section2LengthParagraph = document.getElementById("section2-length-paragraph")
 const section2VolumeParagraph = document.getElementById("section2-volume-paragraph")
 const section2MassParagraph = document.getElementById("section2-mass-paragraph")
 const convertBtn = document.getElementById("convert-btn")
 
 convertBtn.addEventListener("click", function () {
+    
     convertLenth()
     convertVolume()
     convertWeight()
@@ -18,6 +19,7 @@ convertBtn.addEventListener("click", function () {
 
 
 function convertLenth() {
+    const unitsInput = document.getElementById("units-display-area").value
     let distanceFeet = unitsInput * 3.281
     distanceFeet = distanceFeet.toFixed(3)
     let distanceMeters = unitsInput/3.281
@@ -26,6 +28,7 @@ function convertLenth() {
 }
 
 function convertVolume() {
+    const unitsInput = document.getElementById("units-display-area").value
     let volumeGallon = unitsInput * 0.264
     volumeGallon = volumeGallon.toFixed(3)
     let volumeliter = unitsInput / 0.246
@@ -34,6 +37,7 @@ function convertVolume() {
 }
 
 function convertWeight() {
+    const unitsInput = document.getElementById("units-display-area").value
     let weightPounds = unitsInput * 2.204
     weightPounds = weightPounds.toFixed(3)
     let weightInKilograms = unitsInput / 2.204
