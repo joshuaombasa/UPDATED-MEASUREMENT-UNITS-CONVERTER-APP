c304820be41f6c8b35cdfc4f5a4853be29a08dce
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-03-18 12:40:06 +0300

    added elements

---
+++ elements.js
@@ -0,0 +1,5 @@
+const section2LengthParagraph = document.getElementById("section2-length-paragraph")
+const section2VolumeParagraph = document.getElementById("section2-volume-paragraph")
+const section2MassParagraph = document.getElementById("section2-mass-paragraph")
+
+export {section2LengthParagraph, section2VolumeParagraph, section2MassParagraph}
