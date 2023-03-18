5715eb2db4a5635b73a0c0797e9f813467a75616
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-03-18 14:25:18 +0300

    updated elements

--- elements.js
+++ elements.js
@@ -1,5 +1,6 @@
 const section2LengthParagraph = document.getElementById("section2-length-paragraph")
 const section2VolumeParagraph = document.getElementById("section2-volume-paragraph")
 const section2MassParagraph = document.getElementById("section2-mass-paragraph")
+const containerEl = document.getElementById("container")
 
-export {section2LengthParagraph, section2VolumeParagraph, section2MassParagraph}
+export {section2LengthParagraph, section2VolumeParagraph, section2MassParagraph, containerEl}
